6dfc595934b8b51ffc6704ef375202e9c84cb123
Jakub Budzilo <84582825+Striker471@users.noreply.github.com>
2024-12-07 23:12:20 +0100

    flask update

--- send_map.py
+++ send_map.py
@@ -24,4 +24,4 @@ def stream_batch():
     return Response(generate_batches(queue), content_type='application/json')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
