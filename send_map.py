805f6e2144c9158cd03f25994fdf83eaf0127449
Julia1204 <julia.gosciniak1@gmail.com>
2024-12-08 13:04:05 +0100

    create react app and connection

---
+++ send_map.py
@@ -0,0 +1,29 @@
+from flask import Flask, Response
+from multiprocessing import Process, Lock, Queue
+import json
+from flask_cors import CORS
+
+from main import init_fun
+
+app = Flask(__name__)
+CORS(app)
+
+def generate_batches(queue):
+    while True:
+        batch = queue.get()
+        if batch is None:
+            break
+        yield json.dumps(batch) + "\n"
+
+@app.route('/umap_data')
+def stream_batch():
+    queue = Queue()
+    lock = Lock()
+
+    p = Process(target=init_fun, args=(queue, lock))
+    p.start()
+
+    return Response(generate_batches(queue), content_type='application/json')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
